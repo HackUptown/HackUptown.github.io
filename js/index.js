ef810d514814619d18ab6fc206d91e05c9ff6361
Adriana Castaneda <adriana.lcs316@gmail.com>
2017-04-12 12:26:28 -0500

    test ajax 2

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
-$("#submit").on("click", function(){
-	var school = $("#school").val();
-	console.log(school);
-})
+// $("#submit").on("click", function(){
+// 	var email = $("input[name='email']").val();
+// 	var firstname = $("input[name='first']").val();
+// 	var lastname = $("input[name='first']").val();
+// 	var school = $("#school").val();
+// 	console.log(email);
+// 	return false
+// })
 
-// $.ajax({
-// 	type:"post", 
-// 	url:"https://docs.google.com/forms/d/e/1FAIpQLSfB81GCPgZVh9Fa0TGUqiST_WSBG2tnemX3Ao2Gv5n6SIzj0g/formResponse?emailAddress=adr%40gmail.com&entry.237733927=firstname&entry.5893991=lastname&entry.242590037=Goudy&entry.423828352=7th&entry.1379881818=2&entry.653550099=1&entry.318330051=1&entry.2052573965=1&entry.2132472040=1&entry.675507291=money&emailReceipt=true"
-// }).done(function(response){
-// 	console.log(response);
-// });
+$.ajax({
+	type:"post", 
+	url:"https://docs.google.com/forms/d/e/1FAIpQLSfB81GCPgZVh9Fa0TGUqiST_WSBG2tnemX3Ao2Gv5n6SIzj0g/formResponse?emailAddress=adr%40gmail.com&entry.237733927=firstname&entry.5893991=lastname&entry.242590037=Goudy&entry.423828352=7th&entry.1379881818=2&entry.653550099=1&entry.318330051=1&entry.2052573965=1&entry.2132472040=1&entry.675507291=money&emailReceipt=true"
+}).done(function(response){
+	console.log(response);
+});
 
 // var form_url ="https://docs.google.com/forms/d/e/1FAIpQLSfB81GCPgZVh9Fa0TGUqiST_WSBG2tnemX3Ao2Gv5n6SIzj0g/formResponse?"
 // var email = "emailAddress=" + email
@@ -23,6 +27,7 @@ $("#submit").on("click", function(){
 // &entry.2132472040=1
 // &entry.675507291=money
 // &emailReceipt=true
+
 
 
 
